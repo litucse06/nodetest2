a4791a8769efad07c6adac9e206378eefc5c94a6
litucse06 <litu_cse06@yahoo.com>
2018-03-25 11:20:30 +0600

    Add files via upload

---
+++ public/javascripts/global.js
@@ -0,0 +1,262 @@
+// Userlist data array for filling in info box
+var userListData = [];
+
+// DOM Ready =============================================================
+$(document).ready(function() {
+
+    // Populate the user table on initial page load
+    populateTable();
+    
+    // Username link click
+    $('#userList table tbody').on('click', 'td a.linkshowuser', showUserInfo);
+    
+    // Edit User link click
+    $('#userList table tbody').on('click', 'td a.linkediteuser', editUserInfo);
+
+    // Delete User link click
+    $('#userList table tbody').on('click', 'td a.linkdeleteuser', deleteUser);
+
+    // Add User button click
+    $('#btnAddUser').on('click', addUser);
+});
+
+// Functions =============================================================
+
+// Fill table with data
+function populateTable() {
+
+    // Empty content string
+    var tableContent = '';
+
+    // jQuery AJAX call for JSON
+    $.getJSON( '/users/userlist', function( data ) {
+        // Stick our user data array into a userlist variable in the global object
+        userListData = data;
+
+        // For each item in our JSON, add a table row and cells to the content string
+        $.each(data, function(){
+            tableContent += '<tr>';
+            tableContent += '<td><a href="#" class="linkshowuser" rel="' + this.username + '">' + this.username + '</a></td>';
+            tableContent += '<td>' + this.email + '</td>';
+            tableContent += '<td><a href="#" class="linkediteuser" rel="' + this._id + '">Edit</a></td>';
+            tableContent += '<td><a href="#" class="linkdeleteuser" rel="' + this._id + '">delete</a></td>';
+            tableContent += '</tr>';
+        });
+
+        // Inject the whole content string into our existing HTML table
+        $('#userList table tbody').html(tableContent);
+    });
+};
+
+// Show User Info
+function showUserInfo(event) {
+
+    // Prevent Link from Firing
+    event.preventDefault();
+
+    // Retrieve username from link rel attribute
+    var thisUserName = $(this).attr('rel');
+
+    // Get Index of object based on id value
+    var arrayPosition = userListData.map(function(arrayItem) { return arrayItem.username; }).indexOf(thisUserName);
+
+    // Get our User Object
+    var thisUserObject = userListData[arrayPosition];
+
+    //Populate Info Box
+    $('#userInfoName').text(thisUserObject.fullname);
+    $('#userInfoAge').text(thisUserObject.age);
+    $('#userInfoGender').text(thisUserObject.gender);
+    $('#userInfoLocation').text(thisUserObject.location);
+
+};
+// Edit User Info
+function editUserInfo(event) {
+
+    event.preventDefault();
+
+    // Retrieve username from link rel attribute
+    var thisUserId = $(this).attr('rel');
+
+    // Get Index of object based on id value
+    // use chaining with indexOf
+    // Verbose version http://jsfiddle.net/cwbuecheler/2qEZ2/
+
+    // Map our ids 
+    var arrayPosition = userListData.map(function(arrayItem) {
+        return arrayItem._id;
+    }).indexOf(thisUserId);
+
+    // Locate selected user object
+    var thisUserObject = userListData[arrayPosition];
+
+    // Populate Form Fields
+    $('#inputUserName').val(thisUserObject.username);
+    $('#inputUserEmail').val(thisUserObject.email);
+    $('#inputUserFullname').val(thisUserObject.fullname);
+    $('#inputUserAge').val(thisUserObject.age);
+    $('#inputUserLocation').val(thisUserObject.location);
+    $('#inputUserGender').val(thisUserObject.gender);
+    $('#inputUserMobile').val(thisUserObject.mobile);
+    $('#inputUserNid').val(thisUserObject.nid);
+
+    // add the id to the form object
+    $('#addUser').attr('data-uid', thisUserId);
+
+};
+
+
+// Add User
+function addUser(event) {
+    event.preventDefault();
+
+    // Super basic validation - increase errorCount variable if any fields are blank
+    var errorCount = 0;
+    $('#addUser input').each(function(index, val) {
+        if($(this).val() === '') { errorCount++; }
+    });
+
+    // Check and make sure errorCount's still at zero
+    if(errorCount === 0) {
+
+        // If it is, compile all user info into one object
+        var newUser = {
+            'username': $('#addUser fieldset input#inputUserName').val(),
+            'email': $('#addUser fieldset input#inputUserEmail').val(),
+            'fullname': $('#addUser fieldset input#inputUserFullname').val(),
+            'age': $('#addUser fieldset input#inputUserAge').val(),
+            'location': $('#addUser fieldset input#inputUserLocation').val(),
+            'mobile': $('#addUser fieldset input#inputUserMobile').val(),
+            'nid': $('#addUser fieldset input#inputUserNid').val(),
+            'gender': $('#addUser fieldset input#inputUserGender').val()
+        }
+
+        // Use AJAX to post the object to our adduser service
+        $.ajax({
+            type: 'POST',
+            data: newUser,
+            url: '/users/adduser',
+            dataType: 'JSON'
+        }).done(function( response ) {
+
+            // Check for successful (blank) response
+            if (response.msg === '') {
+
+                // Clear the form inputs
+                $('#addUser fieldset input').val('');
+
+                // Update the table
+                populateTable();
+
+            }
+            else {
+
+                // If something goes wrong, alert the error message that our service returned
+                alert('Error: ' + response.msg);
+
+            }
+        });
+    }
+    else {
+        // If errorCount is more than 0, error out
+        alert('Please fill in all fields');
+        return false;
+    }
+};
+// Update User
+function editUser(event) {
+    event.preventDefault();
+
+    // Super basic validation - increase errorCount variable if any fields are blank
+    var errorCount = 0;
+    $('#addUser input').each(function(index, val) {
+        if($(this).val() === '') { errorCount++; }
+    });
+
+    // Check and make sure errorCount's still at zero
+    if(errorCount === 0) {
+
+        // If it is, compile all user info into one object
+        var updateUser = {
+            'uid': $('#addUser').attr('data-uid'),
+            'username': $('#addUser fieldset input#inputUserName').val(),
+            'email': $('#addUser fieldset input#inputUserEmail').val(),
+            'fullname': $('#addUser fieldset input#inputUserFullname').val(),
+            'age': $('#addUser fieldset input#inputUserAge').val(),
+            'location': $('#addUser fieldset input#inputUserLocation').val(),
+            'mobile': $('#addUser fieldset input#inputUserMobile').val(),
+            'nid': $('#addUser fieldset input#inputUserNid').val(),
+            'gender': $('#addUser fieldset input#inputUserGender').val()
+        }
+
+        // Use AJAX to post the object to our adduser service
+        $.ajax({
+            type: 'PUT',
+            data: updateUser,
+            url: '/users/edituser' + updateUser.uid,
+            dataType: 'JSON'
+        }).done(function( response ) {
+
+            // Check for successful (blank) response
+            if (response.msg === '') {
+
+                // Clear the form inputs
+                $('#addUser fieldset input').val('');
+
+                // Update the table
+                populateTable();
+
+            }
+            else {
+
+                // If something goes wrong, alert the error message that our service returned
+                alert('Error: ' + response.msg);
+
+            }
+        });
+    }
+    else {
+        // If errorCount is more than 0, error out
+        alert('Please fill in all fields');
+        return false;
+    }
+};
+
+// Delete User
+function deleteUser(event) {
+
+    event.preventDefault();
+
+    // Pop up a confirmation dialog
+    var confirmation = confirm('Are you sure you want to delete this user?');
+
+    // Check and make sure the user confirmed
+    if (confirmation === true) {
+
+        // If they did, do our delete
+        $.ajax({
+            type: 'DELETE',
+            url: '/users/deleteuser/' + $(this).attr('rel')
+        }).done(function( response ) {
+
+            // Check for a successful (blank) response
+            if (response.msg === '') {
+            }
+            else {
+                alert('Error: ' + response.msg);
+            }
+
+            // Update the table
+            populateTable();
+
+        });
+
+    }
+    else {
+
+        // If they said no to the confirm, do nothing
+        return false;
+
+    }
+
+};
